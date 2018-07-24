8b2f9a3f097047c9905d3a96fa888d613cab9332
gpiskunov <flzp2475>
2018-07-24 18:01:59 +0300

    Add scala read from file example.

---
+++ src/main/scala/example/ReadFileExample.scala
@@ -0,0 +1,15 @@
+
+import  scala.io.Source
+
+object ReadFileExample {
+  def main(args: Array[String]): Unit = {
+    if (args.length > 0) {
+      for (line <- Source.fromFile(args(0)).getLines())
+        println(line.length + " " + line)
+    }
+    else
+      Console.err.println("Please enter filename")
+  }
+
+
+}
